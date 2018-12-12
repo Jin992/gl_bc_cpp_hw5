6ee374bb3d0f732f2d2c0f4eab3de0629cb8c359
jin992 <arteshchuk@gmail.com>
2018-12-13 00:12:02 +0200

    corrected tests in main.cpp
    Signed-off-by: jin992 <arteshchuk@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,25 +11,22 @@ int main() {
 	Port &addition_info2 = vp2;
 
 	std::cout << "\nDerived class check" << std::endl;
-	std::cout << "default : " << vp << " | Show() method: ";
+	std::cout << "default : " << vp << " | BottleCount() method: " << vp.BottleCount() << " | Show() method: ";
 	vp.Show();
 
-	std::cout << "copy    : " << vp1 << " | Show() method: ";
+	std::cout << "copy    : " << vp1 << " | BottleCount() method: " << vp1.BottleCount() << " | Show() method: ";
 	vp1.Show();
 
-	std::cout << "assigned: " << vp2 << " | Show() method: ";
+	std::cout << "assigned: " << vp2 << " | BottleCount() method: " << vp2.BottleCount() << " | Show() method: ";
 	vp1.Show();
 
 	std::cout << "\nBase class check" << std::endl;
-	std::cout << "default : " << addition_info << " | BottleCount() method: ";
-	std::cout << addition_info.BottleCount() << std::endl;
+	std::cout << "default : " << addition_info << std::endl;
 
 
-	std::cout << "copy    : " <<  addition_info1 << " | BottleCount() method: ";
-	std::cout << addition_info1.BottleCount() << std::endl;
+	std::cout << "copy    : " <<  addition_info1 << std::endl;
 
-	std::cout << "assigned: " << addition_info2 << " | BottleCount() method: ";
-	std::cout << addition_info2.BottleCount() << std::endl;
+	std::cout << "assigned: " << addition_info2 << std::endl;
 
 	return 0;
 }
